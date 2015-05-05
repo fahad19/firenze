15a6d61f9ca76dc9d88b2c95c24946c58f153691
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-05-05 23:37:19 +0200

    lib: expose all classes through main lib.

--- src/test/cases/lib.js
+++ src/test/cases/lib.js
@@ -6,6 +6,9 @@ var lib = require('../../index');
 describe('lib', function () {
   it('should have properties', function () {
     lib.should.have.property('Database');
+    lib.should.have.property('Collection');
+    lib.should.have.property('Model');
+
     lib.should.have.property('fixtures');
   });
 });
