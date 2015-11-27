e07880c23c0b915891e00f6106fb7e3364a61999
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-11-28 00:27:56 +0100

    more fixtures.

---
+++ src/test/collections/Cities.js
@@ -0,0 +1,31 @@
+/* eslint-disable new-cap */
+
+var P = require('../../Promise');
+var City = require('../models/City');
+
+module.exports = function (db) {
+  return db.createCollectionClass({
+    table: 'cities',
+
+    modelClass: City,
+
+    alias: 'City',
+
+    displayField: 'title',
+
+    schema: {
+      id: {
+        type: 'increments'
+      },
+      name: {
+        type: 'string'
+      },
+      description: {
+        type: 'text'
+      },
+      population: {
+        type: 'integer'
+      }
+    }
+  });
+};
