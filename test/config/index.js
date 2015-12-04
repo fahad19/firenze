810c8ba0e19977dc6390eaa31a1295032f972af2
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-12-05 00:40:27 +0100

    travis: create database.

--- test/config/index.js
+++ test/config/index.js
@@ -3,11 +3,12 @@ import SqlAdapter from '../../src/adapters/Sql';
 export default {
   adapter: SqlAdapter,
 
-  host: '127.0.0.1',
-  port: 3306,
-  database: 'firenze',
-  user: 'root',
-  password: '',
+  host: process.env.FIRENZE_DB_HOST || '127.0.0.1',
+  user: process.env.FIRENZE_DB_USER || 'root',
+  port: process.env.FIRENZE_DB_PORT || 3306,
+  password: process.env.FIRENZE_DB_PASS || '',
+  database: process.env.FIRENZE_DB || 'firenze',
+
   pool: {
     min: 0,
     max: 5
