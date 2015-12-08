e5b230c13dd5a254b26fc990afdae4f79d1a7c7e
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-12-08 03:32:52 +0100

    remove inline docs.

--- src/adapters/Memory/Adapter.js
+++ src/adapters/Memory/Adapter.js
@@ -6,56 +6,6 @@ import {Adapter} from '../../';
 import Query from './Query';
 import Schema from './Schema';
 
-// ## Usage
-//
-// ### Node.js
-//
-// With [npm](https://npmjs.com):
-//
-// ```
-// $ npm install --save firenze-adapter-memory
-// ```
-//
-// Now you can require it as follows:
-//
-// ```js
-// var f = require('firenze');
-// var Database = f.Database;
-// var MemoryAdapter = require('firenze-adapter-memory');
-//
-// var db = new Database({
-//   adapter: MemoryAdapter
-// });
-// ```
-//
-// ### Browser
-//
-//
-// Or [Bower](http://bower.io):
-//
-// ```
-// $ bower installl --save firenze-adapter-memory
-// ```
-//
-// Can be loaded in your HTML page as follows:
-//
-// ```js
-// <script src="bower_components/lodash/lodash.min.js"></script>
-// <script src="bower_components/async/lib/async.js"></script>
-// <script src="bower_components/bluebird/js/browser/bluebird.min.js"></script>
-// <script src="bower_components/validator-js/validator.min.js"></script>
-//
-// <script src="bower_components/firenze/dist/firenze.min.js"></script>
-// <script src="bower_components/firenze-adapter-memory/dist/firenze-adapter-memory.min.js"></script>
-//
-// <script>
-// // Memory adapter is availble in `firenze.MemoryAdapter`
-// var db = new firenze.Database({
-//   adapter: firenze.MemoryAdapter
-// });
-// </script>
-// ```
-//
 export default class MemoryAdapter extends Adapter {
   constructor(givenOptions = {}) {
     const options = {
