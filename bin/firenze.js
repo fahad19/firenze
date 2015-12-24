248e91008f245a7b3c1966a3d0acf50552c7e934
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-12-24 04:06:54 +0100

    migrations: split bin into multiple files.

--- bin/firenze.js
+++ bin/firenze.js
@@ -1,7 +1,4 @@
 /* eslint-disable no-console, no-unused-expressions */
-var chalk = require('chalk');
-var firenze = require('../');
-
 var yargs = require('yargs')
   .command('migration', 'Show commands availble for migrations')
   .example('$0 migration')
@@ -10,133 +7,10 @@ var yargs = require('yargs')
 var argv = yargs.argv;
 var command = argv._[0];
 
-if (command === 'migration') {
-  yargs.reset()
-    .usage('$0 migration')
-    .command('list', 'List migrations')
-    .command('current', 'Show current migration version')
-    .command('run [name]', 'Run specific migration by name')
-    .command('runAll', 'Run all pending migrations')
-    .command('rollback [name]', 'Roll back specific migration by name')
-    .describe('db', 'Path to database file')
-    .describe('db-name', 'Name of database instance (if path exports multiple named instances)')
-    .describe('directory', 'Directory containing migration files')
-    .describe('table', 'Table for migrations (defaults to `migrations`)')
-    .demand(2, 'Must provide a valid command')
-    .demand('db')
-    .argv;
+var commandModule = require('./' + command);
 
-  var subCommand = argv._[1];
-
-  var dbPath = ['.', '/'].indexOf(argv.db.substr(0, 1)) > -1
-    ? argv.db
-    : ('./' + argv.db);
-  if (dbPath.substr(0, 1) === '.') { dbPath = process.cwd() + '/' + dbPath; }
-
-  var db = require(dbPath);
-  if (argv['db-name']) { db = db[argv['db-name']]; }
-
-  var migrationOptions = db.options.migrations ? db.options.migrations : {};
-  migrationOptions.db = db;
-
-  if (argv.directory) { migrationOptions.directory = process.cwd() + '/' + argv.directory; }
-  if (argv.table) { migrationOptions.table = argv.table; }
-
-  var migration = new firenze.Migration(migrationOptions);
-
-  if (subCommand === 'list') {
-    migration.list()
-      .then(function (list) {
-        if (list.length === 0) {
-          return console.log('No migrations available.');
-        }
-
-        console.log('Migrations list:');
-        console.log('');
-
-        list.forEach(function (item) {
-          var line = '    ' + item.id;
-          if (item.run === false) {
-            line += ' ' + chalk.bgGreen.bold('[new]');
-          }
-
-          console.log(line);
-        });
-      })
-      .catch(function (error) {
-        console.log(chalk.bgRed('Error:') + ' ' + error);
-      })
-      .finally(function () {
-        db.close();
-      });
-  } else if (subCommand === 'current') {
-    migration.current()
-      .then(function (record) {
-        if (!record) {
-          return console.log('No migrations have been run yet.');
-        }
-
-        console.log('Last ran migration is ' + chalk.bold(record.id) + ' on ' + chalk.underline(record.created));
-      })
-      .catch(function (error) {
-        console.log(chalk.bgRed('Error:') + ' ' + error);
-      })
-      .finally(function () {
-        db.close();
-      });
-  } else if (subCommand === 'run') {
-    if (typeof argv._[2] === 'undefined') {
-      console.log(chalk.bgRed('Error:') + ' No name given');
-      db.close();
-
-      return;
-    }
-
-    var name = argv._[2];
-    migration.run(name)
-      .then(function () {
-        console.log('Successfully ran ' + chalk.bold(name));
-      })
-      .catch(function (error) {
-        console.log(chalk.bgRed('Error:') + ' ' + error);
-      })
-      .finally(function () {
-        db.close();
-      });
-  } else if (subCommand === 'runAll') {
-    migration.runAll()
-      .then(function () {
-        console.log('Successfully ran all migrations.');
-      })
-      .catch(function (error) {
-        console.log(chalk.bgRed('Error:') + ' ' + error);
-      })
-      .finally(function () {
-        db.close();
-      });
-  } else if (subCommand === 'rollback') {
-    if (typeof argv._[2] === 'undefined') {
-      console.log(chalk.bgRed('Error:') + ' No name given');
-      db.close();
-
-      return;
-    }
-
-    var name = argv._[2];
-    migration.run(name, 'down')
-      .then(function () {
-        console.log('Successfully rolled back ' + chalk.bold(name));
-      })
-      .catch(function (error) {
-        console.log(chalk.bgRed('Error:') + ' ' + error);
-      })
-      .finally(function () {
-        db.close();
-      });
-  } else {
-    yargs.showHelp();
-    db.close();
-  }
+if (typeof commandModule === 'function') {
+  commandModule(yargs);
 } else {
   yargs.showHelp();
 }
