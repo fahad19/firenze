f1f4424fdeadfcaa22b5fb3a7200305dda83ec2b
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-12-25 23:11:41 +0100

    migrations: ignore loading Migration class for dists.

--- dist/firenze.full.config.js
+++ dist/firenze.full.config.js
@@ -1,7 +1,12 @@
+var webpack = require('webpack');
+
 var filename = __filename.split('/').pop().replace(/\.config\.js/, '.js');
 
 module.exports = {
   entry: __dirname + '/../',
+  plugins: [
+    new webpack.IgnorePlugin(/Migration/)
+  ],
   output: {
     path: __dirname,
     filename: filename,
